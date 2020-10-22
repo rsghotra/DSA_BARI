90573854c50d98862a5b8dc4d52cde50262e8d6c
Rupinder Ghotra <rghotra@blackberry.com>
2020-10-22 18:34:27 -0400

    Merging two sorted arrays
    Change-Id: I9513c12cb3dfbafbb6281ffe7e5733f098ab73b8

--- ArrayIsSorted/main.cpp
+++ ArrayIsSorted/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 using namespace std;
-
 /*
 Checking if given array is already sorted
 */
@@ -13,10 +12,10 @@ struct Array {
     int size;
 };
 
-void Display(Array a) {
+void Display(const Array *a) {
     //as it is passed by value therefore - hence it is an object
-    for(int i=0;i<a.length;i++) {
-        cout<< a.A[i] << " ";
+    for(int i=0;i<a->length;i++) {
+        cout<< a->A[i] << " ";
     }
     cout<<endl;
 }
@@ -60,6 +59,7 @@ void Rearrange(Array* arr) {
     int j=arr->length-1;
     while(i<j) {
         while(arr->A[i] < 0) i++;
+        //||==> Watchout; j--
         while(arr->A[j] >= 0) j--;
         if(i<j) {
             int temp = arr->A[j];
@@ -69,22 +69,61 @@ void Rearrange(Array* arr) {
     }
 }
 
+Array* merge(Array arr1, Array arr2) {
+    // ||==> Watchout; Three indexes will be used. An array will be created
+    Array* arr3 = new Array;
+    int i = 0, j = 0, k = 0;
+    //Loop will stop when one of the list is exhausted.
+    while(i<arr1.length && j<arr2.length) {
+        if(arr1.A[i] < arr2.A[j]) {
+            arr3->A[k] =  arr1.A[i];
+            k++;
+            i++;
+        } else {
+            arr3->A[k] =  arr2.A[j];
+            k++;
+            j++;
+        }
+    }
+    //at this time one of the list must be exhausted
+    while(i < arr1.length) {
+        arr3->A[k] =  arr1.A[i];
+        k++;
+        i++;
+    }
+    while(j < arr2.length) {
+        arr3->A[k] =  arr2.A[j];
+        k++;
+        j++;
+    }
+    //Remember we had not yet set the size or length of array3
+    arr3->length=arr1.length+arr2.length;
+    arr3->size=10;
+    return arr3;
+}
+
 int main() {
     //initialized
     Array arr = {{-6, 3, -3, 10, 5, -7, -9, 12, -4}, 9, 10};
     cout<<">==>Displaying array: "<<endl;
-    Display(arr);
+    Display(&arr);
     if(isSorted(arr)) {
         cout << ">==>isSorted():The array is Sorted."<<endl;
         cout << ">==>insert_in_sorted_array(): Inserting in a sorted array. ";
         insert_in_sorted_array(&arr, 10);
-        Display(arr);
+        Display(&arr);
         insert_in_sorted_array(&arr, 1);
-        Display(arr);
+        Display(&arr);
     } else {
         cout << ">==>isSorted():Array Not Sorted!"<<endl;
     }
     Rearrange(&arr);
-    Display(arr);
+    Display(&arr);
+
+    cout << ">==>Merging two sorted arrays.";
+    Array arr1 = {{1,3,5,7,9}, 5, 10};
+    Array arr2 = {{2,4,6,8,10}, 5, 10};
+    Array* arr3 = merge(arr1, arr2);
+    Display(arr3);
     return 0;   
 }
