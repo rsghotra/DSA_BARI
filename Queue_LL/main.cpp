64b9427d1c179fe493f387e46cfaad9f93cd2c3d
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-11-12 02:39:48 -0500

    Linked List Queue Implementation
    Change-Id: I3b92bd0fd81f747a07d07ff6d45d115bdc74ea03

--- Queue_LL/main.cpp
+++ Queue_LL/main.cpp
@@ -7,26 +7,65 @@ struct Node {
 };
 
 struct Queue {
-    Node* first;
-    Node* last;
+    Node* front;
+    Node* rear;
 };
 
 
 void display(Queue *q) {
-
+    Node* ptr = q->front;
+    while(ptr) {
+        cout << ptr->data << " ";
+        ptr=ptr->next;
+    }
+    cout << endl;
 }
 
 void enqueue(Queue* q, int data) {
+    /*
+        Special Cases:
+            - Queue is full - Heap is full
+            - Queue is empty
 
+    */
+    Node* t = new Node;
+    if(t==0) {
+        cout << "Queue is full because heap is full. Can't enqueue" << endl;
+    } else {
+        t->data = data;
+        t->next = 0;
+        //sp2
+        if(q->front==0)  {
+            //initialize front, rear
+            q->front = q->rear = t;
+        } else {
+            q->rear->next = t;
+            q->rear = t;
+        }
+    }
 }
 
 int dequeue(Queue* q) {
+    /*
+        Special Cases:
+            - When list is empty
+    */
+   int x = -1;
+   if(q->front == 0) {
+        cout << "Queue is empty" << endl;
+   } else {
+       Node* ptr = q->front;
+       q->front = q->front->next;
+       x = ptr->data;
+       ptr=0;
+   }
 
+   return x;
 }
 
 int main() {
     Queue q;
-    q.first = q.last = 0;
+    q.front = q.rear = 0;
     enqueue(&q, 10);
     enqueue(&q, 20);
     enqueue(&q, 30);
@@ -35,6 +74,7 @@ int main() {
     enqueue(&q, 60);
     display(&q);
 
-    cout << "Dequeueing: " << dequeue(&q);
+    cout << "Dequeueing: " << dequeue(&q) << endl;
+    cout << "Dequeueing: " << dequeue(&q) << endl;
     return 0;
 }
