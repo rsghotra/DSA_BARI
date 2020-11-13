5f5dc67f495a472ab53d4049f7cf634e58558b66
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-11-13 01:29:09 -0500

    Implemented Circular Queue in C++ using Arrays
    Change-Id: I95b8284d43c2e5cf325548a06af88fa078ac7293

---
+++ CircularQueue_Array_C++/main.cpp
@@ -0,0 +1,105 @@
+/*
+- Circular Linked List:
+    - PITFALLS:
+        - BOTH FRONT AND REAR STARTS FROM INDEX 0 instead 0f -1 in Linear Queue
+        - FRONT IS ALWAYS EMPTY
+        - DRAMATICALLY INCREASE SPACE UTILIZATION: AT MOST N-1 ELEMENTS COULD BE STORED
+        - CIRCULAR ROATION IS ACHIEVED USING MODULATOR OPERATOR
+        - QUEUE IS FULL: IF (R->NEXT)/SIZE == FRONT ==> THEN Q IS FULL
+        - QUEUE IS EMPTY WHEN (R == F)
+        - EXCELLENT REPRESENTATION OF QUEUE
+        - DISPLAY FUNCTION IS ALSO INTERESTING
+
+    - QUEUES - MOST OPTIMALLY IMPLEMENTED IN CIRCULAR FASHION.
+    - DISPLAY - WATCH OUT
+*/
+#include<iostream>
+using namespace std;
+
+class CQueue {
+    private:
+        int size;
+        int front;
+        int rear;
+        int* Q;
+    public:
+        CQueue(int);
+        ~CQueue();
+        void display();
+        void enqueue(int);
+        int dequeue();
+        bool isEmpty();
+        bool isFull();
+};
+
+CQueue::CQueue(int size) {
+    this->size = size;
+    this->Q = new int[this->size];
+    //watch out we start from zero
+    this->front = 0;
+    this->rear = 0;
+}
+
+CQueue::~CQueue() {
+    delete[] Q;
+}
+
+bool CQueue::isEmpty() {
+    if(this->front == this->rear) return true;
+    else return false;
+}
+
+
+bool CQueue::isFull() {
+    if((this->rear+1)%this->size == this->front) return true;
+    return false;
+}
+
+//watch out how you will display the list
+void CQueue::display() {
+    //because front is alaways empty.
+    int i = this->front+1;
+    while(i != (this->rear+1)%this->size) {
+        cout << this->Q[i] << " ";
+        i = (i+1)%(this->size);   
+    }
+    cout << endl;
+}
+
+void CQueue::enqueue(int x) {
+    if(!isFull()) {
+        this->rear =(this->rear+1)%this->size;
+        Q[rear] = x;
+    } else {
+        cout << "Circular Queue is Full" << endl;
+    }
+}
+
+int CQueue::dequeue() {
+    int x=-1;
+    if(!isEmpty()) {
+        front = (front+1)%size;
+        x = Q[front];
+    } else {
+        cout << "Circular queue is Empty." << endl;
+    }
+    return x;
+}
+
+int main() {
+    int A[] = {1,3,5,7,9};
+    CQueue cq(10);
+    for(int i=0;i<5;i++) {
+        cq.enqueue(A[i]);
+    }
+    cq.display();
+    cq.enqueue(10);
+    for(int i=0;i<5;i++) {
+        cout << "Dequeued from Circular Queue: " << cq.dequeue() << endl;
+    }
+    //underflow
+    cq.dequeue();
+    cq.dequeue();
+    cq.dequeue();
+    return 0;
+}
