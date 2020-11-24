51f3974035b3a953a18e16b96e8dda69f29a46f1
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-11-23 19:52:48 -0500

    Hashing - Quadratic Probing
    Change-Id: I6de2ab94a88f1ef16457051d16284658696f2e4d

--- Hashing_Quadratic/main.cpp
+++ Hashing_Quadratic/main.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 /*
     ==> NEED NOT TO WORRY ABOUT IF THE ELEMENT COULD BE INSERTED OR NOT AS HASH TABLE SIZE IS EQUAL TO INPUT SIZE
-    Closed Hashing - Linear Probing: Collision Resolution Technique - Implemented Using Array
-        Key Points: H'(x) = (H(x) + i)%size; where H(x) = x%size; i = 0,1,2,3
+    Closed Hashing - Quadratic Probing: Collision Resolution Technique - Implemented Using Array
+        Key Points: H'(x) = (H(x) + i*i)%size; where H(x) = x%size; i = 0,1,2,3
             Benefits:
                 1. Load Factor, Lambda <=1
-                2. Resolve Collision BUT PLAGUE WITH PRIMARY CLUSTERING
-                3. Analysis:
-                    - Average Successful Search: (1/lamda)log(1/1-lamdba)
-                    - Average Unsuccessful Search: 1/(1-lmabda)
+                2. Resolve Collision & Primary Clustering
 */
 
-const int SIZE = 5;
+const int SIZE = 10;
 
 //H(x) = x%size
 int Hash(int key) {
@@ -21,15 +18,15 @@ int Hash(int key) {
 }
 
 //you will only call linear probling if Modulus Hash Function Fails
-int LinearProbe(int HT[], int key) {
+int QuadraticProbe(int HT[], int key) {
     int index = Hash(key);
     int i = 0;
     do {
-        if(HT[(index + i)%SIZE]==0) {
-            return ((index+i)%SIZE);
+        if(HT[(index + i*i)%SIZE]==0) {
+            return ((index+i*i)%SIZE);
         }
         i++;
-    } while((index + i)%SIZE !=index); //HT is full
+    } while((index + i*i)%SIZE !=index); //HT is full
     return -1;
 }
 
@@ -37,11 +34,11 @@ int Search(int HT[], int key) {
     int index = Hash(key);
     int i = 0;
     do {
-        if(HT[(index+i)%SIZE] == key) {
-            return ((index+i)%SIZE);
+        if(HT[(index+i*i)%SIZE] == key) {
+            return ((index+i*i)%SIZE);
         }
         i++;
-    } while((index+i)%SIZE != index);
+    } while((index+i*i)%SIZE != index);
     return -1;
 }
 
@@ -50,7 +47,7 @@ void Insert(int HT[], int key) {
     int index = Hash(key);
     if(HT[index] != 0) {
         //call for linear probing
-        index = LinearProbe(HT, key);
+        index = QuadraticProbe(HT, key);
     }
     if( index == -1) {
             cout << "Hash Table is Full." << endl;
@@ -72,11 +69,11 @@ int main() {
     Insert(HT, 12);
     Insert(HT, 25);
     Insert(HT, 35);
-    Insert(HT, 37);
+    Insert(HT, 55);
     Insert(HT, 39);
     Insert(HT, 41);
     PrintHash(HT);
     cout << "Key found at " << Search(HT, 35) << endl;
     cout << "Key found at " << Search(HT, 30) << endl;
-    cout << "Key found at " << Search(HT, 12) << endl;
+    cout << "Key found at " << Search(HT, 55) << endl;
 }
