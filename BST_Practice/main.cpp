5322a3a8dfdecd79e806b066e33e2d4ab98e44d0
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-12-04 01:27:50 -0500

    Insert Operation BST: Recursive and Iterative
    Change-Id: Idd5f616fadf57322392a68f9e59ba8a19aa53b06

--- BST_Practice/main.cpp
+++ BST_Practice/main.cpp
@@ -8,36 +8,69 @@ struct Node {
     int val;
     Node* left;
     Node* right;
+    Node() : val{0}, left{0}, right{0}{}
+    Node(int v) : val{v}, left{0}, right{0}{}
 };
 
 class BST {
     public:
         Node* root;
         BST() {this->root = 0;}
-        void LevelOrder(Node*) const;
+        void InOrder(Node*) const;
         Node* Search(Node*, int) const;
         Node* Search_i(Node*, int) const;
+        Node* Insert(Node*, int);
+        Node* Insert_i(Node*, int);
 };
 
-void BST::LevelOrder(Node* root) const{
-    Node* ptr=0;
-    if(!(root)) return;
-    queue<Node*> Q;
-    Q.push(root);
-    cout << "Level Order: " << endl;
-    while(!Q.empty()) {
-        ptr = Q.front();
-        Q.pop();
+Node* BST::Insert(Node* root, int data) {
+    //in trees recursion termination and insertion happen at leaf node
+    if(root == 0) {
+        Node* temp = new Node(data);
+        return temp;
+    }
+    if(root->val > data) {
+        root->left = Insert(root->left, data);
+    } else if(root->val < data) {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
 
-        cout << ptr->val << " ";
-        if(ptr->left) {
-            Q.push(ptr->left);
-        }
-        if(ptr->right) {
-            Q.push(ptr->right);
+Node* BST::Insert_i(Node* root, int data) {
+    //inorder to insert in tree - two pointers are user - head pointer and tail pointers
+    Node* temp = new Node(data);
+
+    if(root == 0) return temp;
+
+    Node* ptr = root;
+    Node* qtr = 0;
+
+    while(ptr) {
+        qtr = ptr;
+        if(ptr->val > temp->val) {
+            ptr = ptr->left;
+        } else if(ptr->val < temp->val) {
+            ptr = ptr->right;
+        } else {
+            return nullptr;
         }
     }
-    cout << endl;
+
+    if(qtr && qtr->val > temp->val) {
+        qtr->left = temp;
+    } else if(qtr && qtr->val < temp->val) {
+        qtr->right = temp;
+    }
+    return root;
+}
+
+void BST::InOrder(Node* root) const{
+    if(root) {
+        InOrder(root->left);
+        cout << root->val << " ";
+        InOrder(root->right);
+    }
 }
 
 Node* BST::Search(Node* root, int data) const {
@@ -60,4 +93,56 @@ Node* BST::Search_i(Node* root, int data) const {
         else root = root->right;
     }
     return nullptr;
+}
+
+int main() {
+    BST* bst = new BST();
+
+    cout << "\n>==>Creating BST using INSERT." << endl;
+    bst->root = bst->Insert(bst->root, 10);
+    bst->Insert(bst->root, 5);
+    bst->Insert(bst->root, 20);
+    bst->Insert(bst->root, 8);
+    bst->Insert(bst->root, 30);
+    cout << "\n>==>Displaying Created BST - InOrder" << endl;
+    bst->InOrder(bst->root);
+
+    cout << "\n>==>Searching for 5 in BST." << endl;
+    if(bst->Search(bst->root, 5)) 
+        cout << "Element found." << endl;
+    else 
+        cout << "Element not found.\n";
+
+    cout << "\n>==>Searching for 11 in BST." << endl;
+    if(bst->Search(bst->root, 11)) 
+        cout << "Element found." << endl;
+    else 
+        cout << "Element not found.\n";
+    
+    bst->Search_i(bst->root, 10) ? "Element found." : "Element not found.\n";
+
+
+    cout << "INSERT IN A TREE - ITERATIVE\n";
+    bst->root = 0;
+    bst->root = bst->Insert_i(bst->root, 11);
+    bst->Insert_i(bst->root, 13);
+    bst->Insert_i(bst->root, 5);
+    bst->Insert_i(bst->root, 23);
+    bst->Insert_i(bst->root, 29);
+    cout << "\n>==>Displaying Created BST - InOrder" << endl;
+    bst->InOrder(bst->root);
+
+    cout << "\n>==>Searching for 5 in BST." << endl;
+    if(bst->Search(bst->root, 5)) 
+        cout << "Element found." << endl;
+    else 
+        cout << "Element not found.\n";
+
+    cout << "\n>==>Searching for 11 in BST." << endl;
+    if(bst->Search(bst->root, 11)) 
+        cout << "Element found." << endl;
+    else 
+        cout << "Element not found.\n";
+    
+    bst->Search_i(bst->root, 10) ? "Element found." : "Element not found.\n";
 }
